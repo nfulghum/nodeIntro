fee495eb17a180bad6548555932d98d55d811189
Nick Fulghum <nickfulghum92@gmail.com>
2022-09-27 12:38:45 -0500

    step 3

---
+++ step2.js
@@ -0,0 +1,32 @@
+const { default: axios } = require('axios');
+const fs = require('fs');
+const process = require('process');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log("Error reading:", err);
+            process.kill(1)
+        }
+        console.log(data)
+    })
+}
+
+
+async function webCat(url) {
+    try {
+        let res = await axios.get(url);
+        console.log(res.data);
+    } catch (err) {
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1)
+    }
+}
+
+let path = process.argv[2];
+
+if (path.slice(0, 4) === 'http') {
+    webCat(path);
+} else {
+    cat(path);
+}
